971ffe5a9f30d0b1e7b50fb585a33fe00df64334
Paul Standley <paulstandley1972@gmail.com>
2019-05-09 20:22:59 +0100

    setup for react spring

---
+++ src/components/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Page extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <h1>Hello</h1>
+     );
+  }
+}
+ 
+export default Page;
