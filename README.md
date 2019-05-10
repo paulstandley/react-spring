d71bd1f08e22fe9ba1be6b978ed0738de6b5a28b
Paul Standley <paulstandley1972@gmail.com>
2019-05-10 11:32:05 +0100

    React Springs

--- README.md
+++ README.md
@@ -6,4 +6,8 @@
 npm install --save react-spring
 ```
 
+## __[Traversy Media](https://www.youtube.com/watch?v=S8yn3-WpVV8&t=220s)__
+
+![logo](./public/bradslogo.jpg)
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { animated } from "react-spring";
+import { Transition } from 'react-spring/renderprops';
+import Component1 from "./components/Component1";
+import Component2 from "./components/Component2";
+import Component3 from "./components/Component3";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  state = {
+    showComponent3: false
+  };
+
+  toggle = e => this.setState({ showComponent3: !this.state.showComponent3 });
+
+  render() {
+    return (
+      <div className="App">
+        <Component1 />
+        <Component2 toggle={this.toggle} />
+        <Transition
+          native
+          items={this.state.showComponent3}
+          from={{ opacity: 0 }}
+          enter={{ opacity: 1 }}
+          leave={{ opacity: 0 }}
+        >
+          {show =>
+            show &&
+            (props => (
+              <animated.div style={props}>
+                <Component3 />
+              </animated.div>
+            ))
+          }
+        </Transition>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
